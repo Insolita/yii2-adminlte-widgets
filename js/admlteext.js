f7c159c06696eba2a565b3e1b867a15d29059147
insolita <donnainsolita@gmail.com>
2014-11-11 01:52:55 +0800

    unbind previuos click event

--- js/admlteext.js
+++ js/admlteext.js
@@ -1,18 +1,22 @@
-/*
- * Add collapse and remove events to boxes And save cookies
- */
-$("[data-widget='collapse']").off("click");
-$("[data-widget='collapse']").click(function() {
-	var box = $(this).parents(".box").first();
-	//Find the body and the footer
-	var bf = box.find(".box-body, .box-footer");
-	if (!box.hasClass("collapsed-box")) {
-		box.addClass("collapsed-box");
-		$.cookie(box.attr('id')+'_state',"hide");
-		bf.slideUp();
-	} else {
-		box.removeClass("collapsed-box");
-		$.cookie(box.attr('id')+'_state',"show");
-		bf.slideDown();
-	}
+
+$(function() {
+	"use strict";
+	/*
+    * Add collapse and remove events to boxes And save cookies
+    */
+		$("[data-widget='collapse']").off("click");
+		$("[data-widget='collapse']").click(function() {
+			var box = $(this).parents(".box").first();
+			//Find the body and the footer
+			var bf = box.find(".box-body, .box-footer");
+			if (!box.hasClass("collapsed-box")) {
+				box.addClass("collapsed-box");
+				$.cookie(box.attr('id')+'_state',"hide");
+				bf.slideUp();
+			} else {
+				box.removeClass("collapsed-box");
+				$.cookie(box.attr('id')+'_state',"show");
+				bf.slideDown();
+			}
+		});
 });
