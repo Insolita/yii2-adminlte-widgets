256106d8a1aba52317037c0f232cfffcd5f60005
insolita <webmaster100500@ya.ru>
2017-03-25 23:34:05 +0800

    refactor; split andsingle mod support

--- Callout.php
+++ Callout.php
@@ -6,20 +6,42 @@ use \yii\bootstrap\Widget;
 use yii\helpers\Html;
 
 /**
- * This is just an example.
+ * Class Callout
+ * @example
+ * <?php
+ * Callout::widget(['type'=>'info','head'=>'Soma Head','text'=>'some message']);
+ * ?>
+ *
+ * Also possible
+ * <?php Callout::begin(['type'=>'info','head'=>'Soma Head'])?>
+ *    Some body content
+ * <?php Callout::end();?>
+ * @package insolita\wgadminlte
  */
 class Callout extends Widget
 {
+    /**
+     * @deprecated use LteConst instead
+     */
     const TYPE_WARNING = 'warning';
+    /**
+     *@deprecated use LteConst instead
+     */
     const TYPE_DANGER = 'danger';
+    /**
+     *@deprecated use LteConst instead
+     */
     const TYPE_INFO = 'info';
+    /**
+     *@deprecated use LteConst instead
+     */
     const TYPE_DEFAULT = 'default';
 
     /**
      * @var string $type color style of widget
      */
 
-    public $type = self::TYPE_INFO;
+    public $type = LteConst::TYPE_INFO;
 
     /**@var string $head * */
     public $head = '';
@@ -28,21 +50,45 @@ class Callout extends Widget
      * @var string $text your message
      **/
     public $text = '';
+    
+    /**
+     * @var string
+     */
+    public $topTemplate = <<<HTML
+       <div {options}><h4>{head}</h4><p>
+HTML;
+    /**
+     * @var string
+     */
+    public $endTemplate = <<<HTML
+       </p></div>
+HTML;
 
     /**
      * @inheritdoc
      */
     public $options = [];
-
-
-    public function run()
+    
+    public function init()
     {
+        parent::init();
         Html::addCssClass($this->options, 'callout');
         Html::addCssClass($this->options, 'callout-' . $this->type);
-        echo Html::tag(
-            'div', '<h4>' . $this->head . '</h4>'
-            . '<p>' . $this->text . '</p>', $this->options
-        );
+        echo strtr($this->topTemplate, [
+            '{options}'=>Html::renderTagAttributes($this->options),
+            '{head}'=>$this->head
+        ]);
+        if($this->text){
+            echo $this->text;
+        }
+    }
+    
+    /**
+     *
+     */
+    public function run()
+    {
+        echo $this->endTemplate;
     }
 
 }
