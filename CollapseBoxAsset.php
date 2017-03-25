59c210abe0483bc67c4de5138cb2ffa0367b1c93
insolita <webmaster100500@ya.ru>
2017-03-25 21:03:53 +0800

    collapse box

---
+++ CollapseBoxAsset.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Insolita
+ * Date: 11.11.14
+ * Time: 0:50
+ */
+
+namespace insolita\wgadminlte;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class CollapseBoxAsset
+ *
+ * @package insolita\wgadminlte
+ */
+class CollapseBoxAsset extends AssetBundle
+{
+    /**
+     * @var string
+     */
+    public $sourcePath = '@vendor/insolita/yii2-adminlte-widgets/js';
+    
+    /**
+     * @var array
+     */
+    public $js
+        = [
+            'collapsebox.js',
+        ];
+    
+    /**
+     * @var array
+     */
+    public $depends
+        = [
+            'yii\web\YiiAsset',
+            'insolita\wgadminlte\JsCookieAsset',
+        ];
+}
+
